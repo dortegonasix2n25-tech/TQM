d54617a31e58004f87a0cd5f03a6595be97f909c
dortegonasix2n25-tech <dortegon.asix2n25@lamerce.com>
2025-11-25 17:29:47 +0100

    Create game.js

---
+++ game/game.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pare Noel Pixel Game</title>
+    <link rel="stylesheet" href="style.css"> <!-- Estilos del juego -->
+</head>
+<body>
+    <!-- Canvas principal donde se dibujará el juego -->
+    <canvas id="game" width="900" height="500"></canvas>
+
+    <!-- Link al archivo JS del juego -->
+    <script src="game.js"></script> <!-- Lógica del juego -->
+</body>
+</html>
